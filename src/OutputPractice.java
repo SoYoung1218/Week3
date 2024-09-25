b25b9356b0ece3039483bb39300f1793ee598ec7
SoyYoung1218 <ehdudala1930@gmail.com>
2024-09-25 16:24:48 +0900

    Initial commit

---
+++ src/OutputPractice.java
@@ -0,0 +1,17 @@
+public class OutputPractice {
+    public static void main(String[] args) {
+        int age = 20;
+        String hobby = "자는게 제일 좋아";
+        String name = "최소영";
+
+        System.out.println(name + "(" + age + "세) " + hobby);
+
+        System.out.print(name);
+        System.out.print("(");
+        System.out.print(age);
+        System.out.print("세) ");
+        System.out.print(hobby);
+//        System.out.println(age + "세");
+//        System.out.println(hobby);
+    }
+}
